49c364a6eaa199080709b2e60bbf5d9a6e224add
Sebastian Niespodziany <sebastianniespodziany@gmail.com>
2019-05-11 20:58:16 +0200

    prototype iterative local search

---
+++ Code/Methods/IterativeLocalSearch.py
@@ -0,0 +1,18 @@
+
+
+
+class IterativeLocalSearach:
+    def __init__(self):
+        self.ls
+
+    def multiple_start_local_search(self):
+        pass
+
+    def iterative_local_search(self):
+        pass
+
+    def destroy(self):
+        pass
+
+    def repair(self):
+        pass
